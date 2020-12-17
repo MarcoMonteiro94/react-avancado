94369d1210fcb5391153a1a6b9042cb38ae8572e
Marco Monteiro <marcomonteiro94@hotmail.com>
2020-12-17 01:23:24 -0300

    Adicionado editorconfig e eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "React Avançado" }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>React Avançado!</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing{" "}
+          <code className={styles.code}>src/pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -56,10 +58,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
