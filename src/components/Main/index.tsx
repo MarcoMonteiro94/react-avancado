3eebb5725aa494a5715ea0c9c51ebe14d5190f83
Marco Monteiro <marcomonteiro94@hotmail.com>
2020-12-25 23:29:25 -0300

    :sparkles: Added Styled Components in the project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Main = () => {
-  const sayHello = true;
   return (
     <main>
       <h1>React Avançado</h1>
-      {sayHello ? 'Hello' : 'Goodbye'};
     </main>
   );
 };
